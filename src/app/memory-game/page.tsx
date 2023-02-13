978d25138004ff7c4337c955b56a716874d13f87
Mitch Aplin <mitchaplin@gmail.com>
2023-02-13 16:24:54 -0600

    Remove default game size option

--- src/app/memory-game/page.tsx
+++ src/app/memory-game/page.tsx
@@ -138,7 +138,6 @@ export default function InteractiveRating() {
             id="sizes"
             className="block w-full rounded-lg border-gray-600 bg-gray-700 p-2.5 text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500"
           >
-            <option selected>Choose game size</option>
             <option value={2}>2x2</option>
             <option value={4}>4x4</option>
             <option value={6}>6x6</option>
