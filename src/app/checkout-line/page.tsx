51a35b1c321155ebcb79998bc53b9ddefc34a95e
Mitch Aplin <mitchaplin@gmail.com>
2023-02-28 18:01:26 -0600

    checkout line balancing

--- src/app/checkout-line/page.tsx
+++ src/app/checkout-line/page.tsx
@@ -5,35 +5,59 @@ export type Cashier = {
   number: number;
   name: string;
   status: "idle" | "working" | "on break";
-  lane: number;
+  lane: 1 | 2 | 3 | -1;
 };
+
 export type Customer = {
   number: number;
   name: string;
-  items: 3;
+  items: number;
   status: "waiting" | "checkout";
-  line: -1;
+  lane: number;
 };
 
 const getWorkingCondition = (status: Cashier["status"]) => {
   switch (status) {
     case "idle":
-      return "bg-yellow-300";
+      return "bg-yellow-500";
     case "working":
-      return "bg-green-300";
+      return "bg-green-500";
     case "on break":
-      return "bg-red-300";
+      return "bg-red-500";
   }
 };
+
 const generateOptionalCustomer = (customers: Customer[]) => {
   const random = Math.random();
+  let lane1 = customers.reduce(
+    (acc, customer) => (customer.lane === 1 ? acc++ : acc),
+    0
+  );
+  let lane2 = customers.reduce(
+    (acc, customer) => (customer.lane === 2 ? acc++ : acc),
+    0
+  );
+  let lane3 = customers.reduce(
+    (acc, customer) => (customer.lane === 3 ? acc++ : acc),
+    0
+  );
+  console.log(lane1, lane2, lane3);
+  let min;
+  if (lane1 < lane2 && lane1 < lane3) {
+    min = 1;
+  } else if (lane2 < lane1 && lane2 < lane3) {
+    min = 2;
+  } else {
+    min = 3;
+  }
+  console.log(min);
   if (random > 0.5) {
     const newCustomer: Customer = {
       number: customers.length + 1,
       name: `Jill ${customers.length + 1}`,
-      items: 3,
+      items: Math.ceil(Math.random() * 8) + 1,
       status: "waiting",
-      line: -1,
+      lane: min,
     };
     return [...customers, newCustomer];
   }
@@ -48,6 +72,18 @@ export default function CheckoutLine() {
       status: "working",
       lane: 1,
     },
+    {
+      number: 2,
+      name: "Joe",
+      status: "working",
+      lane: 2,
+    },
+    {
+      number: 3,
+      name: "Jim",
+      status: "working",
+      lane: 3,
+    },
   ]);
   const [customers, setCustomers] = useState<Customer[]>([
     {
@@ -55,22 +91,42 @@ export default function CheckoutLine() {
       name: "Jill",
       items: 3,
       status: "waiting",
-      line: -1,
+      lane: 1,
     },
   ]);
-
+  // check if items = 0, if so, remove customer from line
+  // else if person is checking out, remove items by line
   useEffect(() => {
     const interval = setInterval(() => {
       let c = generateOptionalCustomer(customers);
       c && setCustomers(c);
-    }, 5000);
+      if (
+        customers &&
+        customers.length > 0 &&
+        (customers[0].items <= 0 ||
+          customers[1].items <= 0 ||
+          customers[2].items <= 0)
+      ) {
+        if (customers[0].items === 0) customers.splice(0, 1);
+        if (customers[1].items === 0) customers.splice(1, 1);
+        if (customers[2].items === 0) customers.splice(2, 1);
+        setCustomers(customers);
+        return;
+      } else {
+        // remove items from three customers in front on lines
+        customers.find((customer) => customer.lane === 1 && customer)!
+          .items-- || null;
+        customers.find((customer) => customer.lane === 2)!.items-- || null;
+        customers.find((customer) => customer.lane === 3)!.items-- || null;
+      }
+    }, 1000);
     return () => clearInterval(interval);
   }, [customers]);
 
   return (
     <main className="h-screen w-screen overflow-x-auto bg-black p-12 text-white">
       <h1 className="mb-8 text-center text-4xl">Checkout Line</h1>
-      <div className="flex justify-center">
+      <div className="flex items-start justify-center gap-4 transition-all">
         {cashiers.map((cashier) => {
           return (
             <div key={cashier.number} className="grid grid-cols-1 gap-4">
@@ -82,20 +138,26 @@ export default function CheckoutLine() {
               >
                 <div key={cashier.number}>
                   <h3>{cashier.name}</h3>
-                  <h4>{cashier.status}</h4>
+                  {customers.some((cust) => cust.lane === cashier.lane) ? (
+                    <h4>{cashier.status}</h4>
+                  ) : (
+                    <h4>{"idle"}</h4>
+                  )}
                 </div>
               </button>
               {customers.map((customer) => {
-                return (
+                return customer.lane === cashier.lane ? (
                   <button
                     key={customer.number}
                     className={`h-24 w-24 rounded-full bg-blue-300`}
                   >
                     <div key={customer.number}>
                       <h3>{customer.name}</h3>
-                      <h4>{customer.status}</h4>
+                      <h4>{customer.items}</h4>
                     </div>
                   </button>
+                ) : (
+                  <></>
                 );
               })}
             </div>
