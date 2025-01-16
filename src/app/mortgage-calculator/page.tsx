617933db20dd73fe18a1e6820adfd6e8a1b074eb
Mitch Aplin <mitchaplin@gmail.com>
2025-01-15 23:06:57 -0600

    General layout complete

--- src/app/mortgage-calculator/page.tsx
+++ src/app/mortgage-calculator/page.tsx
@@ -9,67 +9,69 @@ export default function MortgageCalculator() {
   const router = useRouter();
   return (
     <main className={styles.main}>
-      <div className={styles.card}>
-        {!submitted ? (
-          <>
-            <button className="rounded-full bg-slate-700 p-3">
-              <Image
-                src={"./assets/interactive-rating/icon-star.svg"}
-                width={12}
-                height={12}
-                alt="card star"
-              ></Image>
-            </button>
-            <div className="pt-8">
-              <h1 className="text-xl text-white">How did we do?</h1>
-              <p className="pt-4 text-gray-400">
-                Please let us know how we did with your support request. All
-                feedback is appreciated to help us improve our offering!
-              </p>
-              <div className="flex flex-grow py-8">
-                {[1, 2, 3, 4, 5].map((star) => (
-                  <button
-                    onClick={() => setRating(star)}
-                    value={star}
-                    key={star}
-                    className={`${
-                      star === rating
-                        ? "bg-orange-500 text-white hover:bg-orange-500"
-                        : "bg-slate-600 text-gray-400 hover:bg-gray-500"
-                    } mx-3 flex h-10 w-10 grow items-center justify-center gap-2 rounded-full`}
-                  >
-                    {star}
-                  </button>
-                ))}
-              </div>
-
-              <button
-                onClick={() => setSubmitted(true)}
-                className="h-10 w-full grow rounded-full bg-orange-500 text-white hover:bg-white hover:text-orange-500"
-              >
-                Submit
-              </button>
-            </div>
-          </>
-        ) : (
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src={"./assets/interactive-rating/illustration-thank-you.svg"}
-              width={225}
-              height={225}
-              alt="card star"
-            ></Image>
-            <button className="mx-auto mt-8 h-10 w-fit cursor-default rounded-full bg-gray-700 px-8 text-orange-500">
-              You selected {rating} out of 5
-            </button>
-            <h1 className="mt-8 text-center text-3xl text-white">Thank you!</h1>
-            <p className="mt-4 text-center text-gray-400">
-              We appreciate you taking the time to give a rating. If you ever
-              need more support, don&apos;t hesitate to get in touch!
-            </p>
+      <form className="w-full max-w-lg bg-slate-100 rounded-md p-4">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="mortgage-amount"
+            >
+              Mortgage Amount
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="mortgage-amount"
+              type="number"
+              placeholder="300,000"
+            />
           </div>
-        )}
-      </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="mortgage-term"
+            >
+              Mortgage Term
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="mortgage-term"
+              type="number"
+              placeholder="30"
+            />
+            {/* <p className="text-red-500 text-xs italic">
+              Please fill out this field.
+            </p> */}
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="interest-rate"
+            >
+              Interest Rate
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="interest-rate"
+              type="number"
+              placeholder="5"
+            />
+          </div>
+
+          <div className="w-full md:w-1/2 px-3">
+            <label className="flex border-gray-700 bg-gray-100 text-gray-700 rounded-md px-3 py-3 my-3  hover:bg-indigo-300 cursor-pointer ">
+              <input className="my-3" type="radio" name="Repayment" />
+              <i className="p-2">Repayment</i>
+            </label>
+
+            <label className="flex border-gray-700 bg-gray-100 text-gray-700 rounded-md px-3 py-3 my-3  hover:bg-indigo-300 cursor-pointer ">
+              <input className="my-3" type="radio" name="Interest" />
+              <i className="p-2">Interest Only</i>
+            </label>
+          </div>
+        </div>
+      </form>
       <button
         onClick={() => {
           router.push("/");
